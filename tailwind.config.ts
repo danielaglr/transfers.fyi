61c25d43d02b9030dbd5174a326ba3c2c96069cb
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-09-25 16:37:05 -0700

    Add css themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'transfers': '#00BF78',
+        'transfers-light': '#E3FAF1',
+        'transfers-dark': '#00AC6D'
+      }
     },
   },
   plugins: [],
