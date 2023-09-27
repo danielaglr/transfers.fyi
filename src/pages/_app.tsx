9a4fa8c57d4e934e7859c8025118e44259200adc
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-09-26 17:14:18 -0700

    Use layout and add global font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Layout from './_layout';
+import { Figtree } from 'next/font/google';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const Fig = Figtree({ subsets: ['latin'] });
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={Fig.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  )
+};
+
+export default App;
