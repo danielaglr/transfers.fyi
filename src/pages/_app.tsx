f6a0239693f42dbcdbd4aa5d620bd2e84d22bac2
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-09-30 18:45:14 -0700

    Add user context provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,19 @@ import type { AppProps } from 'next/app';
 import Layout from './_layout';
 import { Figtree } from 'next/font/google';
 import '@/styles/globals.css';
+import AuthProvider from '@/contexts/UserContext';
 
 const Fig = Figtree({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={Fig.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </main>
+    <AuthProvider>
+      <main className={Fig.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </AuthProvider>
   )
 };
 
