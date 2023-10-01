81d2fd1963426b51033b0e01ccee25ded111dc1b
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-09-30 19:10:54 -0700

    Add firebase config

---
+++ firebase.ts
@@ -0,0 +1,16 @@
+import { getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+if (getApps().length < 1) initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore();
