be2332ce4cc9b830d03d0353361640ba52c61cbb
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-10-02 02:11:22 -0700

    Initialize storage service

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 if (getApps().length < 1) initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore();
+export const sb = getStorage();
