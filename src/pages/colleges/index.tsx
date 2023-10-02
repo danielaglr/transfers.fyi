600f13aafe6e506f2d195e3ad0acc7e3b118bc8c
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-10-02 02:34:23 -0700

    Create college card display
    - Utilize handler function to filter down cards based on MUI autocomplete value
    
    TODO:
    - Utilize logos storage bucket instead of hardcoded logo URLs
    - Implement different filters based on school types and groupings (IVYs, UCs, etc.)

--- src/pages/colleges/index.tsx
+++ src/pages/colleges/index.tsx
@@ -3,13 +3,16 @@ import getColleges from '@/helpers/getColleges';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Autocomplete, TextField } from '@mui/material';
+import { useState } from 'react';
 
 function CollegeCard(props: DocumentData) {
   const { college } = props;
 
   return (
-    <Link href={`/colleges/${college.collegeID}`} className='w-72 h-48 bg-white border border-gray-100 rounded-md shadow-sm'>
-      <Image src={college.collegeLogo} width={128} height={128}  alt={college.collegeName + ' Logo'} />
+    <Link href={`/colleges/${college.collegeID}`} className='flex flex-col justify-center items-center min-w-72 h-48 bg-white border border-gray-100 px-2 rounded-xl shadow-sm'>
+      <div className='flex justify-center items-center h-32'>
+        <Image src={college.collegeLogo} width={128} height={128}  alt={college.collegeName + ' Logo'} />
+      </div>
       <span className='text-gray-800 text-center font-medium'>{college.collegeName}</span>
     </Link>
   )
@@ -20,14 +23,43 @@ interface CollegesProps {
 };
 
 function Colleges({ colleges }: CollegesProps) {
+  const [selectedSchool, setSelectedSchool] = useState<DocumentData | null>(null);
+
+  function handleColleges() {
+    if (selectedSchool !== null) {
+      const filteredColleges = colleges;
+      return filteredColleges.filter(function(college) {
+        return college === selectedSchool;
+      });
+    } else {
+      return colleges
+    }
+  };
+
   return (
-    <div className='flex flex-row justify-between min-h-[calc(100vh_-_100px)] bg-gray-50 p-12'>
-      <div className='flex flex-col max-w-md 2xl:max-w-lg'>
-        <h1 className='text-3xl text-gray-800 text-left font-bold mb-10'>Search or filter for college-specific transfer admission data!</h1>
+    <div className='flex flex-col lg:flex-row justify-between min-h-[calc(100vh_-_100px)] bg-gray-50 pt-10 px-6 lg:p-12'>
+      <div className='flex flex-col lg:max-w-xs xl:max-w-md 2xl:max-w-lg'>
+        <h1 className='text-2xl 2xl:text-3xl text-gray-800 text-center lg:text-left font-bold mb-10'>Search or filter for college-specific transfer admission data!</h1>
       </div>
       <div className='flex flex-1 flex-col pt-2'>
-        <div className=''>
-          <Autocomplete disablePortal options={} renderInput={(params) => <TextField {...params} label="College" />} size='small' />
+        <Autocomplete 
+          disablePortal
+          clearOnEscape
+          fullWidth
+          options={colleges}
+          getOptionLabel={(doc) => doc.collegeName}
+          renderInput={(params) => <TextField {...params} label='Search Schools' />}
+          value={selectedSchool}
+          onChange={(event, newVal) => setSelectedSchool(newVal)}
+          size='small'
+          sx={{ backgroundColor: '#fff', marginBottom: 2, '& label.Mui-focused': { color: '#00AC6D' }, '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: '#00BF78' } } }}
+        />
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4'>
+          {handleColleges().map((item, index) => {
+            return (
+              <CollegeCard key={index} college={item} />
+            )
+          })}
         </div>
       </div>
     </div>
