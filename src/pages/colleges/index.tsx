e08f85798515f50826b6a9d7cfce52445be12e93
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-10-04 16:08:34 -0700

    Rework entire filtering system
    - Utilize checkboxes to toggle a variety of college-related filters
    - Memoize filtered colleges to prevent constant function calls
    
    TODO:
    - Potentially paginate / infinite-scroll data if page load gets too slow / resource intensive

--- src/pages/colleges/index.tsx
+++ src/pages/colleges/index.tsx
@@ -2,21 +2,24 @@ import { DocumentData } from 'firebase/firestore';
 import getColleges from '@/helpers/getColleges';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Autocomplete, Skeleton, TextField } from '@mui/material';
-import { useState } from 'react';
+import { Autocomplete, Checkbox, TextField } from '@mui/material';
+import { useMemo, useState } from 'react';
 
 function CollegeCard(props: DocumentData) {
   const { college } = props;
 
   return (
     <Link href={`/colleges/${college.collegeID}`} className='flex flex-col justify-center items-center min-w-72 h-48 bg-white border border-gray-100 px-2 rounded-xl shadow-sm'>
-      {college.collegeLogo === '' ? 
-        <Skeleton variant='rounded' width={128} height={128} />
-      :
-        <div className='flex justify-center items-center h-32'>
-          <Image src={college.collegeLogo} width={128} height={128} alt={college.collegeName + ' Logo'} />
-        </div>
-      }
+      <div className='flex justify-center items-center h-32'>
+        <Image 
+        priority={true} 
+        src={college.collegeLogo} 
+        width={128}
+        height={128}
+        className='object-contain'
+        alt={college.collegeName + ' Logo'}
+        />
+      </div>
       <span className='text-gray-800 text-center font-medium'>{college.collegeName}</span>         
     </Link>
   )
@@ -28,25 +31,82 @@ interface CollegesProps {
 
 function Colleges({ colleges }: CollegesProps) {
   const [selectedSchool, setSelectedSchool] = useState<DocumentData | null>(null);
+  const [toggleFilters, setToggleFilters] = useState({
+    IVYSchools: true,
+    UCSchools: true,
+    PublicCollege: true,
+    PrivateCollege: true,
+  });
 
   function handleColleges() {
-    if (selectedSchool !== null) {
-      const filteredColleges = colleges;
-      return filteredColleges.filter(function(college) {
-        return college === selectedSchool;
-      });
-    } else {
-      return colleges
-    }
+    let filteredColleges = colleges;
+
+    if (selectedSchool !== null) { 
+      filteredColleges = filteredColleges.filter(college => college === selectedSchool); 
+    };
+
+    filteredColleges = filteredColleges.filter(college => {
+      if (toggleFilters.IVYSchools && college.schoolType === 'Ivy League') {
+        return true;
+      }
+      if (toggleFilters.UCSchools && college.schoolType === 'UC School') {
+        return true;
+      }
+      if (toggleFilters.PublicCollege && college.collegeType === 'Public') {
+        return true;
+      }
+      if (toggleFilters.PrivateCollege && college.collegeType === 'Private') {
+        return true;
+      }
+      return false;
+    });
+
+    return filteredColleges;
   };
+
+  const filteredColleges = useMemo(() => handleColleges(), [colleges, selectedSchool, toggleFilters]);
 
   return (
     <div className='flex flex-col lg:flex-row justify-between h-[calc(100vh_-_100px)] bg-gray-50 pt-10 px-6 lg:p-12 overflow-hidden'>
-      <div className='flex flex-col lg:max-w-xs xl:max-w-md 2xl:max-w-lg'>
-        <h1 className='text-2xl 2xl:text-3xl text-gray-800 text-center lg:text-left font-bold mb-10'>Search or filter for college-specific transfer admission data!</h1>
-        <div className='flex flex-col'>
+      <div className='flex flex-col lg:max-w-xs 2xl:max-w-md 3xl:max-w-lg'>
+        <h1 className='text-xl xl:text-2xl 2xl:text-3xl text-gray-800 text-center lg:text-left font-bold mb-8'>Search or filter for college-specific transfer admission data!</h1>
+        <div className='flex flex-col border-b border-gray-200 pb-4'>
           <span className='text-base text-gray-800 font-semibold'>School Type</span>
-
+          <div className='flex flex-row items-center'>
+            <Checkbox 
+            checked={toggleFilters.IVYSchools} 
+            onChange={() => setToggleFilters({ ...toggleFilters, IVYSchools: !toggleFilters.IVYSchools })}
+            sx={{ '&.Mui-checked': { color: '#00AC6D' } }}
+            />
+            <span className='text-base text-gray-800 font-medium'>Ivy League Schools</span>
+          </div>
+          <div className='flex flex-row items-center'>
+            <Checkbox 
+            checked={toggleFilters.UCSchools} 
+            onChange={() => setToggleFilters({ ...toggleFilters, UCSchools: !toggleFilters.UCSchools })}
+            sx={{ '&.Mui-checked': { color: '#00AC6D' } }}
+            />
+            <span className='text-base text-gray-800 font-medium'>UC Schools</span>
+          </div>
+        </div>
+        <div className='flex flex-col border-b border-gray-200 py-4'>
+          <span className='text-base text-gray-800 font-semibold'>College Type</span>
+          <div className='flex flex-row items-center'>
+            <Checkbox 
+            checked={toggleFilters.PrivateCollege} 
+            onChange={() => setToggleFilters({ ...toggleFilters, PrivateCollege: !toggleFilters.PrivateCollege })}
+            sx={{ '&.Mui-checked': { color: '#00AC6D' } }}
+            />
+            <span className='text-base text-gray-800 font-medium'>Private Colleges</span>
+          </div>
+          <div className='flex flex-row items-center'>
+            <Checkbox 
+            checked={toggleFilters.PublicCollege} 
+            onChange={() => setToggleFilters({ ...toggleFilters, PublicCollege: !toggleFilters.PublicCollege })}
+            sx={{ '&.Mui-checked': { color: '#00AC6D' } }}
+            />
+            <span className='text-base text-gray-800 font-medium'>Public Colleges</span>
+          </div>
         </div>
       </div>
       <div className='flex flex-1 flex-col pt-2 px-4 overflow-hidden'>
@@ -54,7 +114,7 @@ function Colleges({ colleges }: CollegesProps) {
           disablePortal
           clearOnEscape
           fullWidth
-          options={colleges}
+          options={filteredColleges}
           getOptionLabel={(doc) => doc.collegeName}
           renderInput={(params) => <TextField {...params} label='Search Schools' />}
           value={selectedSchool}
@@ -67,7 +127,7 @@ function Colleges({ colleges }: CollegesProps) {
           } }}
         />
         <div className='grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 overflow-y-auto'>
-          {handleColleges().map((item, index) => {
+          {filteredColleges.map((item, index) => {
             return (
               <CollegeCard key={index} college={item} />
             )
